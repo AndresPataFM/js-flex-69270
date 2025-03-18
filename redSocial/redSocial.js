ed0670d5b05771b4b91e938efe099fe3aaba5342
AndresPataFM <apfm21@Gmail.com>
2025-03-18 01:07:30 -0300

    clase 7 más red social

--- redSocial/redSocial.js
+++ redSocial/redSocial.js
@@ -12,84 +12,149 @@ class Comentario{
 
 
 class Usuario{
-  constructor(nombre, edad, mail, comentarios = [], observados = []){
+  constructor({nombre, edad, mail, comentarios = [], observados = []}){
+    // destructuring en parámetros
     this.nombre = nombre
     this.edad = edad
     this.mail = mail
     this.comentarios = []
     this.observados = observados
-    for(const texto in comentarios){
-      const comm = new Comentario(texto, this.nombre)
+    comentarios.forEach(({texto, fecha})=>{
+      const comm = new Comentario(texto, this.nombre, fecha)
       // nunca los pushie
       this.comentarios.push(comm)
+    })
+  }
+  crearCartaUsuario(){
+    const usuariosDOM = document.getElementById("usuarios")
+
+    const carta = document.createElement("article")
+    const nombre = document.createElement("h3")
+    const observados = document.createElement("div")
+    const formComentario = document.createElement("form")
+    const formAlert = document.createElement("div")
+    const formTextarea = document.createElement("textarea")
+    const formSubmit = document.createElement("input")
+    const comentarios = document.createElement("ul")
+
+    // nombre
+    nombre.textContent = this.nombre
+
+    //observados
+    observados.textContent = this.mostrarObservados()
+
+    // formulario de comentarios
+    formTextarea.name = "texto"
+    formTextarea.textContent = "Yo estoy pensando en..."
+    formSubmit.type = "submit"
+    formSubmit.textContent = "Comentar"
+    formComentario.appendChild(formAlert)
+    formComentario.appendChild(formTextarea)
+    formComentario.appendChild(formSubmit)
+
+    // comentarios previos
+    function mostrarComentarios(usuario){
+      comentarios.innerHTML = ""
+      usuario.comentarios.forEach(comentario=>{
+        const comm = document.createElement("li")
+        comm.textContent = comentario.mostrar()
+        comentarios.appendChild(comm)
+      })
     }
+
+    mostrarComentarios(this)
+
+    function agregarComentario(usuario){
+      // logica que evalua el texto y si esta bien crea el comentario
+      const texto = formTextarea.value.trim()
+      if(texto || texto!==""){
+        usuario.crearComentario(texto)
+        formAlert.textContent = ""
+        mostrarComentarios(usuario)
+      } else {
+        formAlert.textContent = "Por favor ingrese un texto válido"
+      }
+    }
+
+    formComentario.addEventListener("submit", evento=>{
+      // evento submit para crear comentario
+      evento.preventDefault()
+      agregarComentario(this)
+    })
+
+    // ensamblo la carta
+    carta.appendChild(nombre)
+    carta.appendChild(observados)
+    carta.appendChild(formComentario)
+    carta.appendChild(comentarios)
+
+    // Agrego la carta al DOM
+
+    usuariosDOM.appendChild(carta)
   }
   crearComentario(textoIngresado){
     // crea comentarios de usuario, los agrega al inicio del array
-    let texto = textoIngresado
+    let texto = textoIngresado.trim() //saco espacios exceso
     // pedimos el texto al usuario
-    if(texto){
-
+    if(texto || texto===""){
+      // creamos el comentario
+      const comentario = new Comentario(texto, this.nombre)
+      // agregamos al array
+      this.comentarios.unshift(comentario)
+      // guardo cambios en storage
+      usuarios.guardarCambios()
     } else {
-      texto = prompt("Ingrese su comentario")
-      while(texto.length >80 || texto === null || texto === ""){
-        // validación del texto
-        texto = prompt("Ingrese su comentario. No puede tener más de 80 carácteres ni apretar cancelar ni estar vacío")
-      }
+      console.warn("texto inválido")
     }
-    // creamos el comentario
-    const comentario = new Comentario(texto, this.nombre)
-    // agregamos al array
-    this.comentarios.unshift(comentario)
   }
   mostrarObservados(){
-    alert(`Los usuarios que tenes observados son: ${this.observados.join("; ")}`)
-  }
-  mostrarComentarios(){
-    let comentariosAMostrar = "Los comentarios en su feed son: "
-    for(let i =0; i<this.comentarios.length; i++){
-      comentariosAMostrar += `\n #${this.comentarios.length - i} - ` +  this.comentarios[i].mostrar()
-    }
-    alert(comentariosAMostrar)
+    return `Los usuarios que tenes observados son: ${this.observados.join("; ")}`
   }
 }
 // propiedades
-const usuarios = []
+const usuarios = {
+  // Aplico Object in object out, facilita el manejo de los usuarios al tenerlos en un único objeto
+  // Esto tranquilamente podría ser un singleton
+  lista: [],
+  guardarCambios: function(){
+    // guarda cambios de la lista de usuarios
+    const guardadoTemp = JSON.stringify(this.lista)
+    localStorage.setItem("redSocial-usuarios", guardadoTemp)
+  },
+  crearUsuario: function({nombre, edad, mail, comentarios, observados}){
+    if(this.lista.some(usuario=>usuario.nombre===nombre)){
+      console.warn("El nombre debe de ser único")
+    } else {
+      const usuario = new Usuario({nombre, edad, mail, comentarios, observados})
+      this.lista.push(usuario)
+      this.guardarCambios()
+    }
+  },
+  cargarUsuarios:function(){
+    const usuariosTemp = JSON.parse(localStorage.getItem("redSocial-usuarios"))
+    if(usuariosTemp) usuariosTemp.forEach(function(usuario){usuarios.crearUsuario(usuario)})
+  }
+}
 
-// funciones
-const crearUsuario = (nombre, edad, mail, comentarios, observados)=>{
-  // crea objetos Usuario y los agrega al array usuarios
-  // const nombre = prompt("Ingrese su nombre")
-  // const edad
-  // const mail
-  // const comentarios
-  // const observados
-  const usuario = new Usuario(nombre, edad, mail, comentarios, observados)
-  usuarios.push(usuario)
+const alCargar = function(){
+  usuarios.cargarUsuarios()
+  if(usuarios.lista.length === 0){
+    usuarios.crearUsuario({nombre:"Andres", edad:32, mail:"asd@coder.com", comentarios:[], observados:["Matias"]})
+    usuarios.crearUsuario({nombre:"Matias", edad:32, mail:"asd@coder.com", comentarios:[{texto:"Primero",fecha: new Date()}], observados:[]})
+    usuarios.crearUsuario({nombre:"Samantha", edad:32, mail:"asd@coder.com", comentarios:[{texto:"Mi primer comentario",fecha: new Date()}, {texto:"Hermoso día",fecha: new Date()}], observados:[]})
+    usuarios.crearUsuario({nombre:"Espia", edad:32, mail:"asd@coder.com", comentarios:[], observados:["Andres", "Matias", "Samantha"]})
+  }
 }
 
 function app(){
-  alert("Bienvenido Andrés")
-  let loop = true
-  while(loop){
-    let opcion = parseInt(prompt("1-crear comentario \n 2-ver comentarios"))
-    if(opcion === 1){
-      usuarios[0].crearComentario()
-    } else if (opcion === 2){
-      usuarios[0].mostrarComentarios()
-    } else {
-      alert("no es una opción correcta")
-    }
-    loop = confirm("Desea tomar otra opción?")
-  }
-  alert("Chau")
+  alCargar()
+  usuarios.lista .forEach(usuario=>{
+    usuario.crearCartaUsuario()
+  })
 }
 
 // Ejecución de código
 
-crearUsuario("Andres", 32, "asd@coder.com", [], ["Matias"])
-crearUsuario("Matias", 32, "asd@coder.com", ["Primero"], [])
-crearUsuario("Samantha", 32, "asd@coder.com", ["Mi primer comentario", "Hermoso día"], [])
-crearUsuario("Espia", 32, "asd@coder.com", [], ["Andres", "Matias", "Samantha"])
+
 
 app()
